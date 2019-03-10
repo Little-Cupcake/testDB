94453df54cafd56dfcdc27ccd4381fc93ee14444
Little-Cupcake <47009437+Little-Cupcake@users.noreply.github.com>
2019-03-10 17:31:04 +0100

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,153 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        void f1()
+        {
+            DataRow workRow = testDataSet.Child_Table.NewRow();
+
+
+            workRow["str_col"] = textBox5.Text;
+
+            int sel_inx = string_ColComboBox.SelectedIndex;
+            DataRow lastRow;
+            DataRow we = testDataSet.ParentTable.Rows[sel_inx];
+             workRow["fk"] = we[0];
+            // int t = Convert.ToInt32(we[0]);
+            
+
+            try { workRow["INTcol"] = Convert.ToInt32(textBox6.Text); }
+            catch (FormatException)
+            {
+                MessageBox.Show("To nie była liczba. Zostanie wpisana liczba 1", "uwaga");
+                textBox6.Clear();
+                workRow["INTcol"] = 1;
+            }
+
+            try
+            {
+
+
+
+                lastRow = testDataSet.Child_Table.AsEnumerable().Last();
+                workRow["Id"] = Convert.ToInt32(lastRow[0]) + 1;
+                // child_TableTableAdapter.Insert(Convert.ToInt32(lastRow[0]) + 1, textBox5.Text, Convert.ToInt32(textBox6.Text), t);
+            }
+
+            catch (NoNullAllowedException)
+            {
+                workRow["Id"] = 1;
+            }
+            catch (InvalidOperationException)
+            {
+                workRow["Id"] = 1;
+            }
+
+            
+            //child_TableTableAdapter.Insert(21, "vse", 23,34 );
+            testDataSet.Child_Table.Rows.Add(workRow);
+            child_TableTableAdapter.Update(testDataSet.Child_Table);
+
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+        void f2()
+        {
+            DataRow workRow = testDataSet.ParentTable.NewRow();
+
+
+            workRow["string_COL"] = textBox3.Text;
+            try
+            {
+                workRow["INT_COL"] = Convert.ToInt32(textBox4.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("To nie była liczba. Zostanie wpisana liczba 1", "uwaga");
+                textBox4.Clear();
+                workRow["INT_COL"] = 1;
+            }
+            try
+            {
+
+                DataRow lastRow = testDataSet.ParentTable.AsEnumerable().Last();
+                workRow["Id"] = Convert.ToInt32(lastRow[0]) + 1;
+            }
+            catch (NoNullAllowedException)
+            {
+                workRow["Id"] = 1;
+            }
+            catch (InvalidOperationException)
+            {
+                workRow["Id"] = 1;
+            }
+
+            testDataSet.ParentTable.Rows.Add(workRow);
+            parentTableTableAdapter.Update(testDataSet.ParentTable);
+            textBox4.Clear();
+            textBox3.Clear();
+        }
+
+        void f3()
+        {
+            DataRow workRow = testDataSet.SingleTable.NewRow();
+            workRow["string"] = textBox2.Text;
+            try
+            {
+                workRow["int"] = Convert.ToInt32(textBox1.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("To nie była liczba. Zostanie wpisana liczba 1", "uwaga");
+                textBox4.Clear();
+                workRow["int"] = 1;
+            }
+            try
+            {
+                
+                DataRow lastRow = testDataSet.SingleTable.AsEnumerable().Last();
+                workRow["Id"] = Convert.ToInt32(lastRow[0]) + 1;
+            }
+            catch (NoNullAllowedException)
+            {
+                workRow["Id"] = 1;
+            }
+            catch (InvalidOperationException)
+            {
+                workRow["Id"] = 1;
+            }
+
+
+            testDataSet.SingleTable.Rows.Add(workRow);
+            singleTableTableAdapter.Update(testDataSet.SingleTable);
+
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        void v()
+        {
+            
+            var g = child_TableDataGridView.CurrentRow.Cells[3].Value;
+            int t = Convert.ToInt32(g);
+            DataRow r = testDataSet.ParentTable.FindById(t);
+            string s = Convert.ToString(r[1]);
+            comboBox1.Text = s;
+        }
+
+        void UpdateAll()
+        {
+            // update database
+            child_TableTableAdapter.Update(testDataSet.Child_Table);
+            parentTableTableAdapter.Update(testDataSet.ParentTable);
+            singleTableTableAdapter.Update(testDataSet.SingleTable);
+            // update dataGridView
+            child_TableDataGridView.Update();
+            parentTableDataGridView.Update();
+            singleTableDataGridView.Update();
+            
+
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +168,8 @@ namespace WindowsFormsApp2
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "testDataSet.Couple". При необходимости она может быть перемещена или удалена.
+            this.coupleTableAdapter.Fill(this.testDataSet.Couple);
             // TODO: данная строка кода позволяет загрузить данные в таблицу "testDataSet.SingleTable". При необходимости она может быть перемещена или удалена.
             this.singleTableTableAdapter.Fill(this.testDataSet.SingleTable);
             // TODO: данная строка кода позволяет загрузить данные в таблицу "testDataSet.ParentTable". При необходимости она может быть перемещена или удалена.
@@ -29,125 +178,152 @@ namespace WindowsFormsApp2
             this.child_TableTableAdapter.Fill(this.testDataSet.Child_Table);
 
         }
-
-        private void N_Single_button_Click(object sender, EventArgs e)
-        {
-            bool i = true;
-            DialogResult result = MessageBox.Show("Czy chesz dodać?", "dodanie sali", MessageBoxButtons.YesNo);
-            {
-
+        // add buttons
+             private void N_Child_button_Click(object sender, EventArgs e)
+             {
+                DialogResult result = MessageBox.Show("Czy chesz dodać?", "dodanie sali", MessageBoxButtons.YesNo);
                 if (result == DialogResult.Yes)
                 {
-                    SingleTableRow workRow = testDataSet.SingleTable.NewSingleTableRow();
-                    workRow["string"] = textBox2.Text;
 
-                    try
-                    {
-                        workRow["int"] = Convert.ToInt32(textBox1.Text);
-                        DataRow lastRow = testDataSet.SingleTable.AsEnumerable().Last();
-                        workRow["Id"] = Convert.ToInt32(lastRow[0]) + 1;
-                    }
-                    catch (NoNullAllowedException)
-                    {
-                        workRow["Id"] = 1;
-                    }
-                    catch (InvalidOperationException)
-                    {
-                        workRow["Id"] = 1;
-                    }
-                    
-
-                    testDataSet.SingleTable.AddSingleTableRow(workRow);
-                    singleTableTableAdapter.Update(testDataSet.SingleTable);
+                    f1();
+                 v();
                 }
 
 
-                textBox1.Clear();
-                textBox2.Clear();
+             }
+             private void N_Single_button_Click(object sender, EventArgs e)
+             {
+
+                   DialogResult result = MessageBox.Show("Czy chesz dodać?", "dodanie sali", MessageBoxButtons.YesNo);
+                   
+ 
+                       if (result == DialogResult.Yes)
+                       {
+
+                        f3();
+                       }
+
+
+
+                   
+             }
+             private void N_Parent_button_Click(object sender, EventArgs e)
+            {
+                DialogResult result = MessageBox.Show("Czy chesz dodać?", "dodanie sali", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    f2();
+                 UpdateAll();
+                    
             }
-
-
-
-        }
-            private void string_ColComboBox_SelectedIndexChanged(object sender, EventArgs e)
+            }
+             private void string_ColComboBox_SelectedIndexChanged(object sender, EventArgs e)
             {
                 string_ColComboBox.DisplayMember = "string_Col";
                 string_ColComboBox.ValueMember = "Id";
             }
 
-            private void N_Child_button_Click(object sender, EventArgs e)
+        //edits
+
+             private void button2_Click(object sender, EventArgs e)// parent
+        {
+               UpdateAll();
+        }
+
+            private void button3_Click(object sender, EventArgs e)// single
             {
-                DialogResult result = MessageBox.Show("Czy chesz dodać?", "dodanie sali", MessageBoxButtons.YesNo);
-                if (result == DialogResult.Yes)
-                {
-                    Child_TableRow workRow = testDataSet.Child_Table.NewChild_TableRow();
-
-
-                    workRow["str_col"] = textBox5.Text;
-
-                    int sel_inx = string_ColComboBox.SelectedIndex;
-                    DataRow we = testDataSet.ParentTable.Rows[sel_inx];
-                    workRow["fk"] = we[0];
-
-
-                    try
-                    {
-
-                        workRow["INTcol"] = Convert.ToInt32(textBox6.Text);
-
-                        DataRow lastRow = testDataSet.Child_Table.AsEnumerable().Last();
-                        workRow["Id"] = Convert.ToInt32(lastRow[0]) + 1;
-                    }
-                    catch (NoNullAllowedException)
-                    {
-                        workRow["Id"] = 1;
-                    }
-                    catch (InvalidOperationException)
-                    {
-                        workRow["Id"] = 1;
-                    }
-                    testDataSet.Child_Table.AddChild_TableRow(workRow);
-                    child_TableTableAdapter.Update(testDataSet.Child_Table);
-
-                    textBox5.Clear();
-                    textBox6.Clear();
-                }
-
-
+              UpdateAll();
+               
             }
 
-            private void N_Parent_button_Click(object sender, EventArgs e)
+             private void button1_Click(object sender, EventArgs e)//child
+             {
+              int sel_inx = comboBox1.SelectedIndex;
+              var g = child_TableDataGridView.CurrentRow.Cells[0].Value;
+              DataRow we = testDataSet.ParentTable.Rows[sel_inx];
+              int SL_ID = Convert.ToInt32(g);
+              DataRow rw = testDataSet.Child_Table.FindById(SL_ID);
+              rw[3] = we[0];
+              UpdateAll();
+
+            
+               
+             }
+        //delete
+            private void Del_Single_button_Click(object sender, EventArgs e)
             {
-                DialogResult result = MessageBox.Show("Czy chesz dodać?", "dodanie sali", MessageBoxButtons.YesNo);
-                if (result == DialogResult.Yes)
+                if (singleTableDataGridView.CurrentRow != null)
                 {
-                    ParentTableRow workRow = testDataSet.ParentTable.NewParentTableRow();
-
-
-                    workRow["string_COL"] = textBox3.Text;
-
-                    try
-                    {
-                        workRow["INT_COL"] = Convert.ToInt32(textBox4.Text);
-                        DataRow lastRow = testDataSet.ParentTable.AsEnumerable().Last();
-                        workRow["Id"] = Convert.ToInt32(lastRow[0]) + 1;
-                    }
-                    catch (NoNullAllowedException)
-                    {
-                        workRow["Id"] = 1;
-                    }
-                    catch (InvalidOperationException)
-                    {
-                        workRow["Id"] = 1;
-                    }
-                    
-                    testDataSet.ParentTable.AddParentTableRow(workRow);
-                    parentTableTableAdapter.Update(testDataSet.ParentTable);
-                    textBox4.Clear();
-                    textBox3.Clear();
+                    singleTableDataGridView.Rows.Remove(singleTableDataGridView.CurrentRow);
                 }
             }
 
-        
+        private void Del_parent_button_Click(object sender, EventArgs e)
+        {
+            if (parentTableDataGridView.CurrentRow != null)
+            {
+                parentTableDataGridView.Rows.Remove(parentTableDataGridView.CurrentRow);
+            }
+        }
+
+        private void del_child_button_Click(object sender, EventArgs e)
+        {
+            if (child_TableDataGridView.CurrentRow != null)
+            {
+                child_TableDataGridView.Rows.Remove(child_TableDataGridView.CurrentRow);
+            }
+        }
+
+        private void child_TableDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void child_TableDataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            v();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //
+            // string str = Convert.ToString(listBox2.SelectedItem);
+            int t_Single = listBox2.SelectedIndex;
+            DataRow rw_Single = testDataSet.SingleTable.Rows[t_Single];
+            int Id_Single = Convert.ToInt32(rw_Single[0]);
+            /////////////////////////////////////////////////////////////
+            int t_Parent = listBox3.SelectedIndex;
+            DataRow rw_Parent = testDataSet.ParentTable.Rows[t_Parent];
+            int Id_parent = Convert.ToInt32(rw_Parent[0]);
+            //////////////////////////////////////////////////////////////
+            DataRow lastRow;
+            DataRow workRow = testDataSet.Couple.NewRow();
+
+            workRow["fk_Single"] = Id_Single;
+            workRow["fk_Parent"] = Id_parent;
+            try
+            {
+                lastRow = testDataSet.Couple.AsEnumerable().Last();
+                workRow["Id"] = Convert.ToInt32(lastRow[0]) + 1;
+            }
+
+            catch (NoNullAllowedException)
+            {
+                workRow["Id"] = 1;
+            }
+            catch (InvalidOperationException)
+            {
+                workRow["Id"] = 1;
+            }
+            testDataSet.Couple.Rows.Add(workRow);
+            coupleTableAdapter.Update(testDataSet.Couple);
+            UpdateAll();
+            //////////////////////////////
+            string Item1 =Convert.ToString(rw_Single[1]);
+            string Item2 = Convert.ToString(rw_Parent[1]);
+            listBox1.Items.Add(Item1 + " | | " + Item2);
+
+        }
     }
 }
+
