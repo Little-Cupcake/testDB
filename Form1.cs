74dbb11e12ce62470f86b2b3473ed5c6957ef3c3
Little-Cupcake <47009437+Little-Cupcake@users.noreply.github.com>
2019-03-13 16:52:14 +0100

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -14,26 +14,42 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
-        void f1()
+        int Rand_For_U()
         {
+            int rand;
+            var x = new Random();
+            rand = Convert.ToInt32(x.Next(-1, 150));
+            return rand;
+        }
+     void f1()
+        {  
             DataRow workRow = testDataSet.Child_Table.NewRow();
 
-
+            
             workRow["str_col"] = textBox5.Text;
-
+            
+            
+            
+                
             int sel_inx = string_ColComboBox.SelectedIndex;
             DataRow lastRow;
-            DataRow we = testDataSet.ParentTable.Rows[sel_inx];
-             workRow["fk"] = we[0];
+            
+             
             // int t = Convert.ToInt32(we[0]);
+            
+                DataRow we = testDataSet.ParentTable.Rows[sel_inx];
+                workRow["fk"] = we[0];
+                
+            
             
 
             try { workRow["INTcol"] = Convert.ToInt32(textBox6.Text); }
             catch (FormatException)
             {
-                MessageBox.Show("To nie była liczba. Zostanie wpisana liczba 1", "uwaga");
+                int x = Rand_For_U() ;
+                MessageBox.Show("To nie była liczba. Zostanie wpisana liczba " + x , "uwaga");
                 textBox6.Clear();
-                workRow["INTcol"] = 1;
+                workRow["INTcol"] = x;
             }
 
             try
@@ -75,9 +91,10 @@ namespace WindowsFormsApp2
             }
             catch (FormatException)
             {
-                MessageBox.Show("To nie była liczba. Zostanie wpisana liczba 1", "uwaga");
+                int x = Rand_For_U();
+                MessageBox.Show("To nie była liczba. Zostanie wpisana liczba " + x, "uwaga");
                 textBox4.Clear();
-                workRow["INT_COL"] = 1;
+                workRow["INT_COL"] = x;
             }
             try
             {
@@ -110,9 +127,10 @@ namespace WindowsFormsApp2
             }
             catch (FormatException)
             {
-                MessageBox.Show("To nie była liczba. Zostanie wpisana liczba 1", "uwaga");
+                int x = Rand_For_U();
+                MessageBox.Show("To nie była liczba. Zostanie wpisana liczba" + x, "uwaga");
                 textBox4.Clear();
-                workRow["int"] = 1;
+                workRow["int"] = x;
             }
             try
             {
@@ -139,13 +157,21 @@ namespace WindowsFormsApp2
 
         void v()
         {
+            try
+            {
+                var g = child_TableDataGridView.CurrentRow.Cells[3].Value;
+                int t = Convert.ToInt32(g);
+                DataRow r = testDataSet.ParentTable.FindById(t);
+                string s = Convert.ToString(r[1]);
+                comboBox1.Text = s;
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("w tej tabeli nie ma elementów. Dodaj nowe dane i sprubój ponownie");
+            }
             
-            var g = child_TableDataGridView.CurrentRow.Cells[3].Value;
-            int t = Convert.ToInt32(g);
-            DataRow r = testDataSet.ParentTable.FindById(t);
-            string s = Convert.ToString(r[1]);
-            comboBox1.Text = s;
         }
+    
 
         void UpdateAll()
         {
@@ -180,14 +206,37 @@ namespace WindowsFormsApp2
         }
         // add buttons
              private void N_Child_button_Click(object sender, EventArgs e)
-             {
+             { bool b = true;
+        
+               
+           
+                Main:if (b) {
                 DialogResult result = MessageBox.Show("Czy chesz dodać?", "dodanie sali", MessageBoxButtons.YesNo);
                 if (result == DialogResult.Yes)
                 {
+                    if (textBox5.Text != "")
+                    {
+                        try {
+                            f1();
+                            v();
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            MessageBox.Show("w tablicy Rodziecielskiej nie ma wpisu do wyboru");
+                            b = false;
+                            textBox5.Clear();
+                            textBox6.Clear();
+                            goto Main;
+                              
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("wprowadź tekst");
+                    }
 
-                    f1();
-                 v();
                 }
+            }
 
 
              }
@@ -195,15 +244,20 @@ namespace WindowsFormsApp2
              {
 
                    DialogResult result = MessageBox.Show("Czy chesz dodać?", "dodanie sali", MessageBoxButtons.YesNo);
-                   
- 
-                       if (result == DialogResult.Yes)
-                       {
 
-                        f3();
-                       }
+            if (result == DialogResult.Yes)
+            {
+                if (textBox5.Text != "")
+                {
+                  f3();
 
+                }
+                else
+                {
+                    MessageBox.Show("wprowadź tekst");
+                }
 
+            }
 
                    
              }
@@ -212,9 +266,16 @@ namespace WindowsFormsApp2
                 DialogResult result = MessageBox.Show("Czy chesz dodać?", "dodanie sali", MessageBoxButtons.YesNo);
                 if (result == DialogResult.Yes)
                 {
-                    f2();
-                 UpdateAll();
-                    
+                    if (textBox5.Text != "")
+                    {
+                        f2();
+                        UpdateAll();
+                    }
+                else
+                {
+                    MessageBox.Show("wprowadź tekst");
+                }
+
             }
             }
              private void string_ColComboBox_SelectedIndexChanged(object sender, EventArgs e)
@@ -222,69 +283,7 @@ namespace WindowsFormsApp2
                 string_ColComboBox.DisplayMember = "string_Col";
                 string_ColComboBox.ValueMember = "Id";
             }
-
-        //edits
-
-             private void button2_Click(object sender, EventArgs e)// parent
-        {
-               UpdateAll();
-        }
-
-            private void button3_Click(object sender, EventArgs e)// single
-            {
-              UpdateAll();
-               
-            }
-
-             private void button1_Click(object sender, EventArgs e)//child
-             {
-              int sel_inx = comboBox1.SelectedIndex;
-              var g = child_TableDataGridView.CurrentRow.Cells[0].Value;
-              DataRow we = testDataSet.ParentTable.Rows[sel_inx];
-              int SL_ID = Convert.ToInt32(g);
-              DataRow rw = testDataSet.Child_Table.FindById(SL_ID);
-              rw[3] = we[0];
-              UpdateAll();
-
-            
-               
-             }
-        //delete
-            private void Del_Single_button_Click(object sender, EventArgs e)
-            {
-                if (singleTableDataGridView.CurrentRow != null)
-                {
-                    singleTableDataGridView.Rows.Remove(singleTableDataGridView.CurrentRow);
-                }
-            }
-
-        private void Del_parent_button_Click(object sender, EventArgs e)
-        {
-            if (parentTableDataGridView.CurrentRow != null)
-            {
-                parentTableDataGridView.Rows.Remove(parentTableDataGridView.CurrentRow);
-            }
-        }
-
-        private void del_child_button_Click(object sender, EventArgs e)
-        {
-            if (child_TableDataGridView.CurrentRow != null)
-            {
-                child_TableDataGridView.Rows.Remove(child_TableDataGridView.CurrentRow);
-            }
-        }
-
-        private void child_TableDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            
-        }
-
-        private void child_TableDataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
-        {
-            v();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
+             private void button4_Click(object sender, EventArgs e)
         {
             //
             // string str = Convert.ToString(listBox2.SelectedItem);
@@ -314,6 +313,10 @@ namespace WindowsFormsApp2
             catch (InvalidOperationException)
             {
                 workRow["Id"] = 1;
+            }catch(DeletedRowInaccessibleException)
+            {
+                workRow["Id"] = 1;
+
             }
             testDataSet.Couple.Rows.Add(workRow);
             coupleTableAdapter.Update(testDataSet.Couple);
@@ -321,9 +324,159 @@ namespace WindowsFormsApp2
             //////////////////////////////
             string Item1 =Convert.ToString(rw_Single[1]);
             string Item2 = Convert.ToString(rw_Parent[1]);
-            listBox1.Items.Add(Item1 + " | | " + Item2);
+            listBox1.Items.Add(Item1);
+            listBox4.Items.Add(Item2);
 
+        }
+
+        //edits
+
+             private void button2_Click(object sender, EventArgs e)// parent
+        {
+               UpdateAll();
+        }
+
+             private void button3_Click(object sender, EventArgs e)// single
+            {
+              UpdateAll();
+               
+            }
+
+             private void button1_Click(object sender, EventArgs e)//child
+             {
+              int sel_inx = comboBox1.SelectedIndex;
+              var g = child_TableDataGridView.CurrentRow.Cells[0].Value;
+              DataRow we = testDataSet.ParentTable.Rows[sel_inx];
+              int SL_ID = Convert.ToInt32(g);
+              DataRow rw = testDataSet.Child_Table.FindById(SL_ID);
+              rw[3] = we[0];
+              UpdateAll();
+
+            
+               
+             }
+
+             private void button5_Click(object sender, EventArgs e)
+             {
+                 try
+                 {
+                      int t_Single = listBox1.SelectedIndex;
+                      DataRow rw_single = testDataSet.Couple.Rows[t_Single];
+                      int Id_single = Convert.ToInt32(rw_single[2]);
+                      ///////////////////////////////////////////////////
+                      int t_Parent = listBox5.SelectedIndex;
+                      DataRow rw_Parent = testDataSet.ParentTable.Rows[t_Parent];
+                      int Id_parent = Convert.ToInt32(rw_Parent[0]);
+                      string Item2 = Convert.ToString(rw_Parent[1]);
+                      rw_single[2] = Id_parent;
+                      UpdateAll();
+                 }
+                 catch (IndexOutOfRangeException)
+                 {
+                    MessageBox.Show("znaczenie nie zostało wybrane");
+                 }
+
+             }
+
+             private void button6_Click(object sender, EventArgs e)
+            {
+               try
+               {
+                int t_Parent = listBox4.SelectedIndex;
+                  DataRow rw_parent = testDataSet.Couple.Rows[t_Parent];
+                  int Id_parent = Convert.ToInt32(rw_parent[2]);
+                  ///////////////////////////////////////////////////
+                  int t_Single = listBox5.SelectedIndex;
+                  DataRow rw_Single = testDataSet.SingleTable.Rows[t_Single];
+                  int Id_single = Convert.ToInt32(rw_Single[0]);
+                  string Item2 = Convert.ToString(rw_Single[1]);
+                  rw_parent[1] = Id_single;
+                  UpdateAll();
+               }
+                  catch (IndexOutOfRangeException)
+               {
+                   MessageBox.Show("znaczenie nie zostało wybrane");
+               }
+        }
+
+        //delete
+        private void Del_Single_button_Click(object sender, EventArgs e)
+            {
+                if (singleTableDataGridView.CurrentRow != null)
+                {
+                    singleTableDataGridView.Rows.Remove(singleTableDataGridView.CurrentRow);
+
+                UpdateAll();
+                }
+            }
+
+        private void Del_parent_button_Click(object sender, EventArgs e)
+        {
+            if (parentTableDataGridView.CurrentRow != null)
+            {
+                parentTableDataGridView.Rows.Remove(parentTableDataGridView.CurrentRow);
+                UpdateAll();
+            }
+        }
+
+        private void del_child_button_Click(object sender, EventArgs e)
+        {
+            if (child_TableDataGridView.CurrentRow != null)
+            {
+                child_TableDataGridView.Rows.Remove(child_TableDataGridView.CurrentRow);
+                UpdateAll();
+            }
+        }
+
+        private void child_TableDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // it's here by mistake
+        }
+        // Show information in editing area.
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int t_Single = listBox2.SelectedIndex;
+            DataRow rw_Single = testDataSet.SingleTable.Rows[t_Single];
+            int Id_Single = Convert.ToInt32(rw_Single[0]);
+            string Item1 = Convert.ToString(rw_Single[1]);
+            textBox7.Text = Item1;
+    
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            int t_Parent = listBox3.SelectedIndex;
+            DataRow rw_Parent = testDataSet.ParentTable.Rows[t_Parent];
+            int Id_parent = Convert.ToInt32(rw_Parent[0]);
+            string Item2 = Convert.ToString(rw_Parent[1]);
+            textBox8.Text = Item2;
+
+        }
+
+        private void child_TableDataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            v();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (listBox4.SelectedItem == null) { 
+            int l1 = listBox1.SelectedIndex;
+                 coupleDataGridView.Rows.RemoveAt(l1);
+                listBox1.Items.RemoveAt(l1);
+                listBox4.Items.RemoveAt(l1);
+                textBox7.Clear();
+                textBox8.Clear();
+            } else
+            {
+                int l4 = listBox4.SelectedIndex;
+                coupleDataGridView.Rows.RemoveAt(l4);
+                listBox1.Items.RemoveAt(l4);
+                listBox4.Items.RemoveAt(l4);
+                textBox7.Clear();
+                textBox8.Clear();
+            }
         }
     }
 }
-
